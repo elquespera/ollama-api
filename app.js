fb4a76e75ecb5321ebbe1dba5b361050c3ac36fb
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2024-03-30 14:11:58 +0100

    refactor: separate out hmac verification

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import { Readable } from "stream";
-import "dotenv/config";
-import { createHash, createHmac } from "crypto";
+import { verifyHMAC } from "./hmac.js";
 
 const ollamaURL = "http://localhost:11434/api";
-const hmacSecret = process.env.API_HMAC_SECRET;
 
 const app = express();
 app.use(express.json({ limit: "50mb" }));
@@ -19,14 +18,12 @@ app.post("/api/ollama/:route", async (request, response) => {
   const { route } = request.params;
   console.log("ollama:", route);
 
-  const isAuth = checkHMAC(
+  const isAuth = verifyHMAC(
     request.headers.authorization,
     `/api/ollama/${route}`,
     "POST",
     request.body
   );
-
-  console.log(isAuth);
 
   if (!isAuth) {
     response.status(401);
@@ -64,27 +61,3 @@ app.post("/api/ollama/:route", async (request, response) => {
 app.listen(port, () => {
   console.log("Server Listening on PORT:", port);
 });
-
-function checkHMAC(authorization, url, method, body) {
-  if (!authorization) return false;
-  const [name, rest] = authorization.split(" ");
-  if (name !== "HMAC") return false;
-
-  const [time, ...restDigest] = rest.split(":");
-
-  const digest = restDigest.join(":");
-
-  const hmac = createHmac("sha256", hmacSecret);
-
-  hmac.update(time);
-  hmac.update(method);
-  hmac.update(url);
-
-  if (body) {
-    const contentHash = createHash("md5");
-    contentHash.update(JSON.stringify(body));
-    hmac.update(contentHash.digest("hex"));
-  }
-
-  return digest === hmac.digest("hex");
-}
